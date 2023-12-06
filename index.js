64c4dbcc1b54fc3a31e2bbe7a6b54dacd2a9e346
3x10RaiseTo8 <abhisheks1177@gmail.com>
2023-12-07 04:44:33 +0530

    Update previous commit

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(people);
+        response.json(person);
       } else {
         response.status(404).end();
       }
