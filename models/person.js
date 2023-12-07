9adaf6cf2f696cf18c3f168b1f0f3726a8346ed9
3x10RaiseTo8 <abhisheks1177@gmail.com>
2023-12-08 01:55:20 +0530

    Add mongoose data validation: ex 3.19

--- models/person.js
+++ models/person.js
@@ -8,11 +8,15 @@ console.log("Connecting to the url:", uri);
 
 mongoose
   .connect(uri)
-  .then((result) => console.log("Connected to the database"))
+  .then((result) => console.log("Connected to the database", uri.slice(0, 7)))
   .catch((error) => console.log("Connection failed", error));
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 });
 
